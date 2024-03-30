f30a4c62e188c194676b58c359bb87a9a4302d62
Nicholas D. Socci <soccin@mskcc.org>
2024-03-30 18:23:23 -0400

    Tweaked freq of logging

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -3,7 +3,7 @@
 II=1
 while [ 1 ]; do
     w | fgrep averag | tee -a W.log;
-    ps -eo pid,user,cmd | fgrep socci | egrep -wv "sshd|grep|bash|ps|wc|tee" | tee PS.${II}.log | wc -l | tee -a PSN.log;
+    ps -eo pid,user,cmd | fgrep socci | egrep -wv "sshd|grep|bash|ps|wc|tee" | tee PS.$((II/100)).log | wc -l | tee -a PSN.log;
     sleep 30
     II=$((II+1))
 done
