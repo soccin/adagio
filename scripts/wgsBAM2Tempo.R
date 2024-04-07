46c7949ebfa4a159c213aa8e198504b8d98ed0b1
Nicholas D. Socci <soccin@mskcc.org>
2024-04-07 13:20:20 -0400

    Fix SAMPLE id and use .bam.bai for index

--- scripts/wgsBAM2Tempo.R
+++ scripts/wgsBAM2Tempo.R
@@ -1,6 +1,6 @@
 get_sm_tag<-function(bam) {
     hh=Rsamtools::scanBamHeader(bam)[[1]]
-    gsub("SM:","",grep("SM:",hh$text[names(hh$text)=="@RG"][1][[1]],value=T))
+    gsub("SM:","",grep("SM:",hh$text[names(hh$text)=="@RG"][1][[1]],value=T)) |> gsub("^s_","",x=_)
 }
 
 argv=commandArgs(trailing=T)
@@ -9,7 +9,7 @@ mapping=tibble::tibble(
     SAMPLE=purrr::map_vec(argv,get_sm_tag),
     TARGET="wgs",
     BAM=fs::path_real(argv),
-    BAI=fs::path_real(gsub("\\.bam$",".bai",argv))
+    BAI=fs::path_real(gsub("\\.bam$",".bam.bai",argv))
 )
 
 readr::write_tsv(mapping,"mapping_bam_tempo.tsv")
