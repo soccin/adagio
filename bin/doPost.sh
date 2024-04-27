31968bebd1a5247cdab1505115d43849749eb351
Nicholas D. Socci <soccin@mskcc.org>
2024-04-27 16:54:02 -0400

    Fix git tag and add facets plots

--- bin/doPost.sh
+++ bin/doPost.sh
@@ -10,9 +10,12 @@ cp $(ls -rt run/*/*/timeline.html | tail -1) post/pipeline_info
 
 Rscript $RDIR/scripts/report01.R
 
+mkdir -p post/plots/facets
+cp $(find out -name '*purity.CNCF.png') post/plots/facets
+
 CMD_LOG=post/pipeline_info/version.txt
 
-GTAG=$(git --git-dir=$RDIR/.git --work-tree=$RDIR describe --all --long --tags --dirty="-UNCOMMITED" --always)
+GTAG=$(git --git-dir=$RDIR/.git --work-tree=$RDIR describe --long --tags --dirty="-UNCOMMITED" --always)
 GURL=$(git --git-dir=$RDIR/.git --work-tree=$RDIR config --get remote.origin.url)
 
 cp out/*/runlog/cmd.sh.log post/pipeline_info
