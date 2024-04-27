d771a39ee98f74460f91e49125d15c4ef91fd3c9
Nicholas D. Socci <soccin@mskcc.org>
2024-04-27 17:46:24 -0400

    Copy output docs to post folder

--- bin/doPost.sh
+++ bin/doPost.sh
@@ -19,6 +19,7 @@ GTAG=$(git --git-dir=$RDIR/.git --work-tree=$RDIR describe --long --tags --dirty
 GURL=$(git --git-dir=$RDIR/.git --work-tree=$RDIR config --get remote.origin.url)
 
 cp out/*/runlog/cmd.sh.log post/pipeline_info
+cp $RDIR/docs/output.html post/pipeline_info
 
 cat <<-END_VERSION > $CMD_LOG
 DATE: $(date)
