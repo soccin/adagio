d6ad593cfac1f1506cba3c874ae97668ef3d1424
Nicholas D. Socci <soccin@mskcc.org>
2024-04-26 21:07:57 -0400

    Fix paths in post script

--- bin/doPost.sh
+++ bin/doPost.sh
@@ -2,25 +2,26 @@
 
 set -eu
 SDIR="$( cd "$( dirname "$0" )" && pwd )"
-
+RDIR=$(realpath $SDIR/..)
 
 mkdir -p post/pipeline_info
 cp $(ls -rt run/*/*/report.html | tail -1) post/pipeline_info
 cp $(ls -rt run/*/*/timeline.html | tail -1) post/pipeline_info
 
-Rscript $SDIR/scripts/filter01.R
+Rscript $RDIR/scripts/filter01.R
 
 CMD_LOG=post/pipeline_info/version.txt
 
-GTAG=$(git --git-dir=$SDIR/.git --work-tree=$SDIR describe --all --long --tags --dirty="-UNCOMMITED" --always)
-GURL=$(git --git-dir=$SDIR/.git --work-tree=$SDIR config --get remote.origin.url)
+GTAG=$(git --git-dir=$RDIR/.git --work-tree=$RDIR describe --all --long --tags --dirty="-UNCOMMITED" --always)
+GURL=$(git --git-dir=$RDIR/.git --work-tree=$RDIR config --get remote.origin.url)
+
+cp out/*/runlog/cmd.sh.log post/pipeline_info
 
 cat <<-END_VERSION > $CMD_LOG
 DATE: $(date)
-SDIR: $SDIR
+SDIR: $RDIR
 GURL: $GURL
 GTAG: $GTAG
 END_VERSION
 
-cp out/*/runlog/cmd.sh.log post/pipeline_info
 
