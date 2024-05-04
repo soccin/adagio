4528a86c9c073d09178324b01b7f9adeab0dd83a
Nicholas D. Socci <soccin@mskcc.org>
2024-05-04 12:58:40 -0400

    Report 01 for SV's

---
+++ scripts/multiSVReport01.R
@@ -0,0 +1,57 @@
+argv=commandArgs(trailing=T)
+if(len(argv)<1) {
+    cat("\n\tusage: multiSVReport.R SV_SOMATIC.bedpe\n\n")
+    quit()
+}
+require(tidyverse)
+
+SVFILE=argv[1]
+vcf=read_tsv(SVFILE,comment="##")
+
+infoParse=vcf %>%
+    select(TUMOR_ID,NORMAL_ID,NAME_A,INFO_A) %>%
+    separate_rows(INFO_A,sep=";") %>%
+    filter(grepl("Call|SVTYPE|SVLEN",INFO_A)) %>%
+    separate(INFO_A,c("KEY","VAL"),sep="=") %>%
+    spread(KEY,VAL)
+
+tbl1=left_join(vcf,infoParse) %>%
+    rename(CHROM_A=`#CHROM_A`) %>%
+    select(-INFO_A,-FORMAT,-TUMOR,-NORMAL,-NAME_A,-NAME_B) %>%
+    select(TUMOR_ID,NORMAL_ID,Callers,NumCallers,NumCallersPass,SVTYPE,everything()) %>%
+    arrange(desc(NumCallers),desc(NumCallersPass),TUMOR_ID) %>%
+    type_convert
+
+projectNo=gsub("Proj_","",grep("^Proj",strsplit(getwd(),"/")[[1]],value=T))
+if(len(projectNo)==0) {
+    projectNo=strsplit(getwd(),"/")[[1]]
+    projectNo=projectNo[len(projectNo)]
+}
+
+headers=grep("^##(INFO)",readLines(SVFILE),value=T)
+
+infoTbl=list()
+for(hi in headers) {
+    column=str_extract(hi,"ID=(.*?),",group=T)
+    description=str_extract(hi,"Description=\"(.*)\"",group=T)
+    if(column %in% colnames(tbl1)) {
+        infoTbl[[len(infoTbl)+1]]=tibble(Column=column,Description=description)
+    }
+}
+
+docs=readLines("iAnnoteSVColumns.txt")
+for(di in docs) {
+    dis=strsplit(di," : ")[[1]]
+    column=dis[1]
+    description=dis[2] %>% gsub(",$","",.)
+    if(column %in% colnames(tbl1)) {
+        infoTbl[[len(infoTbl)+1]]=tibble(Column=column,Description=description)
+    }
+}
+
+infoTbl=bind_rows(infoTbl)
+
+openxlsx::write.xlsx(
+    list(SVTable=tbl1,Glossary=infoTbl),
+    cc("proj",projectNo,"tempoSVCalls.xlsx")
+)
