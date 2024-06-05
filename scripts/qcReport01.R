5b29244e283879ab81978353c0148c926fa369c6
Nicholas D. Socci <soccin@mskcc.org>
2024-06-05 18:12:00 -0400

    Deal with 1 sample case

--- scripts/qcReport01.R
+++ scripts/qcReport01.R
@@ -2,28 +2,74 @@ require(tidyverse)
 require(ggsci)
 
 projectNo=basename(fs::dir_ls("out"))
-d1=read_tsv("out/B-101-860-a/cohort_level/default_cohort/concordance_qc.txt")
-d2=read_tsv("out/B-101-860-a/cohort_level/default_cohort/contamination_qc.txt")
+cohortDir=file.path("out",projectNo,"cohort_level")
+sampleDir=file.path("out",projectNo,"somatic")
 
-pg2=d2 %>%
-    mutate(Pair=gsub("__","\n",Pair)) %>%
-    ggplot(aes(Pair,Contamination,fill=Sample_Type)) +
+
+dir_ls<-function(dir,re) {
+    fs::dir_ls(dir,recur=T,regex=re)
+}
+
+
+read_tsv_quiet<-function(fileName) {
+    read_tsv(fileName,show_col_types = FALSE,progress=F)
+}
+
+sampDat=read_tsv_quiet(dir_ls(cohortDir,"sample_data.txt"))
+nSamps=nrow(sampDat)
+
+if(nSamps==1) {
+
+    d2=read_tsv_quiet(dir_ls(sampleDir,"contamination.txt"))
+
+    d1=read_tsv_quiet(dir_ls(sampleDir,"concordance.txt")) %>%
+        rename(Sample_ID=concordance) %>%
+        rename(Concordance=2) %>%
+        mutate(QC=case_when(Concordance<90 ~ "FAIL", Concordance<95 ~ "WARN", T ~ "PASS"))
+
+    pg1=d1 %>% ggplot(aes(Sample_ID,Concordance,fill=QC)) +
+        theme_light(14) +
+        geom_col() +
+        coord_flip() +
+        scale_fill_nejm() +
+        labs(title=projectNo,subtitle="Conpair - Concordance")
+
+    pg2=d2 %>% ggplot(aes(Sample_ID,Contamination,fill=Sample_Type)) +
         theme_light(14) +
         geom_col(position="dodge") +
         coord_flip() +
-        scale_y_log10() +
         scale_fill_jama() +
         labs(title=projectNo,subtitle="Conpair - Contamination")
 
-pg1=d1 %>%
-    mutate(Pair=gsub("__","\n",Pair)) %>%
-    mutate(QC=case_when(Concordance<90 ~ "FAIL", Concordance<95 ~ "WARN", T ~ "PASS")) %>%
-    ggplot(aes(Pair,Concordance,fill=QC)) +
-    theme_light(14) +
-    geom_col() +
-    coord_flip() +
-    scale_fill_nejm() +
-    labs(title=projectNo,subtitle="Conpair - Concordance")
+
+
+}else{
+
+    d1=read_tsv_quiet(dir_ls(cohortDir,"concordance_qc.txt"))
+    d2=read_tsv_quiet(dir_ls(cohortDir,"contamination_qc.txt"))
+
+    pg2=d2 %>%
+        mutate(Pair=gsub("__","\n",Pair)) %>%
+        ggplot(aes(Pair,Contamination,fill=Sample_Type)) +
+            theme_light(14) +
+            geom_col(position="dodge") +
+            coord_flip() +
+            scale_y_log10() +
+            scale_fill_jama() +
+            labs(title=projectNo,subtitle="Conpair - Contamination")
+
+    pg1=d1 %>%
+        mutate(Pair=gsub("__","\n",Pair)) %>%
+        mutate(QC=case_when(Concordance<90 ~ "FAIL", Concordance<95 ~ "WARN", T ~ "PASS")) %>%
+        ggplot(aes(Pair,Concordance,fill=QC)) +
+        theme_light(14) +
+        geom_col() +
+        coord_flip() +
+        scale_fill_nejm() +
+        labs(title=projectNo,subtitle="Conpair - Concordance")
+
+
+}
 
 pdf(file=cc("Proj",projectNo,"qcRpt01.pdf"),width=11,height=8.5)
 print(pg1)
