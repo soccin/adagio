76ef8a05aa25f51995c5dc063cd4c4e408615bdf
Nicholas D. Socci <soccin@mskcc.org>
2024-08-19 17:14:52 -0400

    Added Germline report

---
+++ scripts/reportGerm01.R
@@ -0,0 +1,54 @@
+require(tidyverse)
+argv=commandArgs(trailing=T)
+mafFiles=argv
+
+maf=map(mafFiles,read_tsv,comment="#") %>%
+    bind_rows %>%
+    mutate(t_var_freq=t_alt_count/t_depth)
+
+tbl1=maf %>%
+    mutate(GPos=paste0(Chromosome,":",Start_Position,"-",End_Position)) %>%
+    select(
+        Sample=Tumor_Sample_Barcode,Gene=Hugo_Symbol,Type=Variant_Classification,
+        dbSNP_RS,Alteration=HGVSp_Short,
+        VAF=t_var_freq,
+        n_depth,n_alt_count,
+        GPos,REF=Reference_Allele,ALT=Tumor_Seq_Allele2
+    ) %>%
+    arrange(Gene,Sample)
+
+class(tbl1$VAF)="percentage"
+
+numMutations=tbl1 %>% count(Sample,name="NumMutations") %>% arrange(desc(NumMutations))
+nSamples=distinct(tbl1,Sample) %>% nrow
+
+library(openxlsx)
+# set zoom
+set_zoom <- function(sV,x) gsub('(?<=zoomScale=")[0-9]+', x, sV, perl = TRUE)
+
+wb=createWorkbook()
+styleHeader=createStyle(wrapText = TRUE, halign="left", textDecoration = c("bold"))
+
+#
+# Sheet 3 - MAF0
+#
+
+addWorksheet(wb,sheetName="Mutations")
+writeDataTable(wb,sheet=1,tbl1,tableStyle="none",withFilter=F)
+addStyle(wb,sheet=1,cols=1:ncol(tbl1),row=1,style=styleHeader,gridExpand=T)
+wb$worksheets[[1]]$sheetViews=set_zoom(wb$worksheets[[1]]$sheetViews,120)
+setColWidths(wb,sheet=1,cols=1:ncol(tbl1),widths="auto")
+setColWidths(wb,sheet=1,cols=1,widths=12)
+setColWidths(wb,sheet=1,cols=5:6,widths=14)
+
+projNo=grep("^Proj",strsplit(getwd(),"/")[[1]],value=T)
+if(len(projNo)==0) {
+    projNo=""
+}
+rFile=cc(projNo,"ReportGermline","v1.xlsx")
+rDir="post/reports"
+fs::dir_create(rDir)
+
+saveWorkbook(wb,file.path(rDir,rFile),overwrite=T)
+
+
