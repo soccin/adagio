c6615aa0d68bf21c024ad2cbe6c5ed4b5c79e46a
Nicholas D. Socci <soccin@mskcc.org>
2025-03-18 19:39:47 -0400

    feat(scripts): add customizable coverage parameters
    Make tumor and normal coverage parameters configurable through
    command line arguments in downSampleArgs.R script:
    
    - Change argument validation to allow optional parameters
    - Add TCOV (default: 60) and NCOV (default: 30) parameters
    - Update usage message to document new parameters
    - Replace hardcoded coverage values with configurable variables
    - Add parameter parsing logic for 3rd and 4th command line arguments

--- scripts/downSampleArgs.R
+++ scripts/downSampleArgs.R
@@ -1,8 +1,8 @@
 suppressPackageStartupMessages({require(tidyverse)})
 
 argv=commandArgs(trailing=T)
-if(len(argv)!=2) {
-    cat("\n\n   usage: downSampleArgs fileOfBamPaths metricsDir\n\n")
+if(len(argv)<2) {
+    cat("\n\n   usage: downSampleArgs fileOfBamPaths metricsDir [TCOV NCOV]\n\n")
     quit()
 }
 
@@ -10,13 +10,22 @@ bams=tibble(BAM=scan(argv[1],"",quiet=T)) %>%
     mutate(SAMPLE=basename(dirname(BAM))) %>%
     mutate(BAM=fs::path_expand(BAM))
 
+TCOV=60
+NCOV=30
+if(!is.na(argv[3])){
+    TCOV=as.numeric(argv[3])
+}
+if(!is.na(argv[4])){
+    NCOV=as.numeric(argv[4])
+}
+
 cov=fs::dir_ls(argv[2],recur=T,regex="\\.wgs.txt$") %>%
     map(read_tsv,comment="#",n_max=1,show_col_types = FALSE,progress=F) %>%
     bind_rows(.id="SAMPLE") %>%
     mutate(SAMPLE=basename(dirname(SAMPLE))) %>%
     select(SAMPLE,MEAN_COVERAGE) %>%
     mutate(TYPE=ifelse(grepl("-N$",SAMPLE),"Normal","Tumor")) %>%
-    mutate(P=pmin(1,ifelse(TYPE=="Normal",30,60)/MEAN_COVERAGE))
+    mutate(P=pmin(1,ifelse(TYPE=="Normal",NCOV,TCOV)/MEAN_COVERAGE))
 
 cmdargs=left_join(bams,cov,by = join_by(SAMPLE)) %>% select(BAM,P)
 
