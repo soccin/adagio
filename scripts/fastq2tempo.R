2eb311565cc7634ececeb5ab973daaba1a194db9
Nicholas D. Socci <soccin@mskcc.org>
2025-07-04 17:21:09 -0400

    Refactor fastq2tempo.R to improve input handling and mapping generation. Introduce error checks for missing R2 files and support for optional target manifest. Add new fastqDir2Tempo.R script for directory-based FASTQ file processing with R1-R2 consistency checks.

--- scripts/fastq2tempo.R
+++ scripts/fastq2tempo.R
@@ -1,17 +1,47 @@
 argv=commandArgs(trailing=T)
-sampleIdRegEx=argv[1]
-seqFiles=scan(argv[2],"")
 
-mapping=list()
-for(si in seqFiles) {
-    f12=strsplit(si,",")[[1]]
-    sid=stringr::str_extract(f12[1],sampleIdRegEx,group=T)
-    mapping[[len(mapping)+1]]=tibble::tibble(
-                                    SAMPLE=sid,TARGET="wgs",
-                                    FASTQ_PE1=fs::path_real(f12[1]),
-                                    FASTQ_PE2=fs::path_real(f12[2])
-                                )
+if(len(argv)<2) {
+    cat("\n\tfastq2tempo.R R1_MAP.tsv BIC_PAIRING.txt [TARGET_MANIFEST.csv]\n\n")
+    quit()
 }
 
-readr::write_tsv(dplyr::bind_rows(mapping),"mapping_fastq_tempo.tsv")
+r1mapFile=argv[1]
+pairingFile=argv[2]
+
+if(len(argv)>=3) {
+    targetFile=argv[3]
+} else {
+    targetFile=NULL
+}
+
+require(tidyverse)
+
+mapping=read_tsv(r1mapFile,col_names=F) %>%
+    rename(SAMPLE=X1,FASTQ_PE1=X2) %>%
+    mutate(FASTQ_PE1=fs::path_real(FASTQ_PE1)) %>%
+    mutate(FASTQ_PE2=str_replace(FASTQ_PE1,"_R1_(\\d+).fastq---","_R2_\\1.fastq"))
+
+if(any(!fs::file_exists(mapping$FASTQ_PE2))) {
+    cat("\n\nERROR missing R2 files\n\n")
+    rlang::abort("ERROR")
+}
+
+if(is.null(targetFile)) {
+
+    mapping$TARGET="idt_v2"
+
+} else {
+
+    targets=read_csv(targetFile,col_names=c("SAMPLE","TARGET"))
+    mapping=mapping %>% left_join(targets)
+
+}
+
+mapping=mapping %>% select(SAMPLE,TARGET,everything())
+
+write_tsv(mapping,"inputTempo_mapping.tsv")
+
+pairing=read_tsv(pairingFile,col_names=c("NORMAL_ID","TARGET_ID"))
+
+write_tsv(pairing,"inputTempo_pairing.tsv")
 
